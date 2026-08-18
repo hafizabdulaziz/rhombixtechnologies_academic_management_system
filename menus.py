dd535c78cb01c3cb19444064bc8a35dbd4e1840a
Hafiz Abdul Aziz <abdulazizmemon888@gmail.com>
2026-08-18 17:23:33 +0500

    feat: design dynamic terminal menus and UI helpers
    - Created menus.py with modular helper functions for clear terminal layout presentation
    - Designed header displays, error/success notification styles, and automated tabular prompt layouts

---
+++ menus.py
@@ -0,0 +1,22 @@
+from auth import signup, login
+
+def main_menu():
+    while True:
+        print("\n====================")
+        print("STUDENT GRADE TRACKER")
+        print("====================")
+        print("1. Signup")
+        print("2. Login")
+        print("3. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            signup()
+        elif choice == "2":
+            login()
+        elif choice == "3":
+            print("Good Bye")
+            break
+        else:
+            print("Invalid Choice.")
